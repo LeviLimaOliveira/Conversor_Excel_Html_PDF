b1ada58960737311d5760cfcfab649552ff98f96
Levi Oliveira <lev.developersr1@gmail.com>
2024-08-08 22:33:43 -0300

    add: PDFWriter

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var Writer = require("./Writer");
 var Processor = require("./Processor");
 var Table = require("./Table");
 var HtmlParser = require("./HtmlParser")
+var PDFWriter = require("./PDFWriter")
 
 var leitor = new Reader();
 var escritor = new Writer();
@@ -17,6 +18,7 @@ async function main(){
   var html = await HtmlParser.Parse(usuarios);
 
   escritor.Write(Date.now() + ".html",html);
+  PDFWriter.WritePDF(Date.now() + ".PDF",html);
 }
 
 main();
